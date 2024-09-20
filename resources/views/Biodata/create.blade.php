f7e021a9232af3474512777505994b28f6f5076a
risarahayu <rrisa329@gmail.com>
2024-09-20 10:27:11 +0800

    initial commit

---
+++ resources/views/Biodata/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Formulir Biodata</h1>
+        <form method="POST" action="{{ route('biodata.store') }}">
+            @csrf
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama</label>
+                <input type="nama" class="form-control" id="" aria-describedby="" placeholder="Tambahkan nama" name="nama" required>
+                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+            </div>
+            <div class="mb-3">
+                <label for="keterangan" class="form-label">Keterangan</label>
+                <textarea type="keterangan" class="form-control" id="" aria-describedby="" placeholder="Tambahkan keterangan" name="keterangan" required></textarea>
+                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    
+@endsection
