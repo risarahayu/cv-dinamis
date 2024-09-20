a437148c892d74007bfbcb4b3d2887bedcce356e
risarahayu <rrisa329@gmail.com>
2024-09-20 10:47:00 +0800

    menambahkan migrasi tabel biodata

--- database/migrations/2024_09_19_063120_create_biodatas_table.php
+++ database/migrations/2024_09_19_063120_create_biodatas_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // menambahkan tabel biodata
         Schema::create('biodatas', function (Blueprint $table) {
             $table->id();
             $table->string('nama'); // Menggunakan tipe string (VARCHAR)
